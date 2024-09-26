3e4d390380bd066bb707705785df0ae1d289ca6c
Jesus Lara <jesuslarag@gmail.com>
2024-09-27 01:48:03 +0200

    fix loaders

--- parrot/version.py
+++ parrot/version.py
@@ -3,7 +3,7 @@
 __title__ = "ai-parrot"
 __description__ = "Live Chatbots based on Langchain chatbots and Agents \
     Integrated into Navigator Framework or used into aiohttp applications."
-__version__ = "0.3.14"
+__version__ = "0.3.15"
 __author__ = "Jesus Lara"
 __author_email__ = "jesuslarag@gmail.com"
 __license__ = "MIT"
