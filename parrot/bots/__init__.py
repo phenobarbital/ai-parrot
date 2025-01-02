d35015270ff842bcdce689591ca400ea84c329ce
Jesus Lara <jesuslarag@gmail.com>
2025-01-02 22:34:01 +0100

    ai parrot

--- parrot/bots/__init__.py
+++ parrot/bots/__init__.py
@@ -3,6 +3,10 @@ from .basic import BasicBot
 from .troc import AskTROC
 from .chatbot import Chatbot
 from .hrbot import HRAgent
+from .cody import Cody
+from .odoo import OddieBot
+from .bose import BoseBot
+
 
 __all__ = (
     'AbstractBot',
