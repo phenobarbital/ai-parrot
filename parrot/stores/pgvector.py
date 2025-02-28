47b884ae628764b175326bbc7de1fdeb8a64807e
Jesus Lara <jesuslarag@gmail.com>
2025-03-01 00:00:07 +0100

    vector stores

--- parrot/stores/pgvector.py
+++ parrot/stores/pgvector.py
@@ -1,5 +1,8 @@
-from typing import Union, Optional
+from typing import List, Union, Optional
 from collections.abc import Callable
+from langchain.docstore.document import Document
+from langchain.memory import VectorStoreRetrieverMemory
+from langchain_community.vectorstores.utils import DistanceStrategy
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
 from langchain_postgres.vectorstores import PGVector
 from .abstract import AbstractStore
@@ -56,9 +59,7 @@ class PgvectorStore(AbstractStore):
         self,
         collection: Union[str, None] = None,
         embedding: Optional[Callable] = None,
-        metadata_field: str = 'id',
-        text_field: str = 'text',
-        vector_key: str = 'vector',
+        **kwargs
     ) -> PGVector:
 
         if not collection:
@@ -77,5 +78,89 @@ class PgvectorStore(AbstractStore):
             logger=self.logger,
             use_jsonb=True,
             create_extension=True,
+            async_mode=True,
+            **kwargs
+        )
+
+    def memory_retriever(
+        self,
+        documents: Optional[List[Document]] = None,
+        num_results: int  = 5
+    ) -> VectorStoreRetrieverMemory:
+        _embed_ = self._embed_ or self.create_embedding(
+                embedding_model=self.embedding_model
+        )
+        vectordb = PGVector.from_documents(
+            documents or {},
+            embedding=_embed_,
+            connection=self._connection,
+            collection_name=self.collection_name,
+            embedding_length=self.dimension,
+            use_jsonb=True,
+            create_extension=True,
             async_mode=True
         )
+        retriever = PGVector.as_retriever(
+            vectordb,
+            search_kwargs=dict(k=num_results)
+        )
+        return VectorStoreRetrieverMemory(retriever=retriever)
+
+    async def from_documents(
+        self,
+        documents: List[Document],
+        collection: Union[str, None] = None,
+        **kwargs
+    ) -> None:
+        """Save Documents as Vectors in VectorStore."""
+        _embed_ = self._embed_ or self.create_embedding(
+                embedding_model=self.embedding_model
+        )
+        if not collection:
+            collection = self.collection_name
+        vectordb = await PGVector.afrom_documents(
+            documents,
+            connection=self._connection,
+            collection_name=collection,
+            embedding=_embed_,
+            embedding_length=self.dimension,
+            use_jsonb=True,
+            create_extension=True,
+            async_mode=True,
+            distance_strategy=DistanceStrategy.EUCLIDEAN_DISTANCE
+        )
+        return vectordb
+
+    async def add_documents(
+        self,
+        documents: List[Document],
+        collection: Union[str, None] = None,
+        **kwargs
+    ) -> None:
+        """Save Documents as Vectors in VectorStore."""
+        _embed_ = self._embed_ or self.create_embedding(
+                embedding_model=self.embedding_model
+        )
+        if not collection:
+            collection = self.collection_name
+        vectordb = self.get_vector(collection=collection, embedding=_embed_)
+        # Asynchronously add documents to PGVector
+        await vectordb.aadd_documents(documents)
+
+    async def similarity_search(
+        self,
+        query: str,
+        collection: Union[str, None] = None,
+        limit: int = 2,
+        filter: Optional[dict] = None
+    ) -> List[Document]:
+        """Search for similar documents in VectorStore."""
+        if collection is None:
+            collection = self.collection_name
+        async with self:
+            vector_db = self.get_vector(collection=collection)
+            return await vector_db.asimilarity_search(
+                query,
+                k=limit,
+                filter=filter
+            )
