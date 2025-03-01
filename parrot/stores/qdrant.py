31c421c86cbbf7c73c381ea1c2262827c4aed740
Jesus Lara <jesuslarag@gmail.com>
2025-03-01 13:56:21 +0100

    new qdrant vector store

--- parrot/stores/qdrant.py
+++ parrot/stores/qdrant.py
@@ -1,9 +1,9 @@
 from collections.abc import Callable
-from typing import Any, Union
-from qdrant_client import QdrantClient  # pylint: disable=import-error
-from langchain_community.vectorstores import (  # pylint: disable=import-error, E0611
-    Qdrant
-)
+from typing import Union, Optional
+from qdrant_client import QdrantClient, models
+from langchain.docstore.document import Document
+from langchain.memory import VectorStoreRetrieverMemory
+from langchain_qdrant import Qdrant
 from .abstract import AbstractStore
 from ..conf import (
     QDRANT_PROTOCOL,
@@ -16,95 +16,155 @@ from ..conf import (
 
 
 class QdrantStore(AbstractStore):
-    """QdrantStore class.
+    """Qdrant DB Store Class.
 
+    Using Qdrant as a Document Vector Store.
 
-    Args:
-        host (str): Qdrant host.
-        port (int): Qdrant port.
-        index_name (str): Qdrant index name.
     """
-    def _create_qdrant_client(self, host, port, url, https, verify, qdrant_args):
+
+    def __init__(
+        self,
+        embedding_model: Union[dict, str] = None,
+        embedding: Union[dict, Callable] = None,
+        **kwargs
+    ):
+        super().__init__(
+            embedding_model=embedding_model,
+            embedding=embedding,
+            **kwargs
+        )
+        self.client: Optional[QdrantClient] = None
+        self.host: str = kwargs.pop('host', QDRANT_HOST)
+        self.port: int = kwargs.pop('port', QDRANT_PORT)
+        self.use_https: bool = kwargs.pop('use_https', QDRANT_USE_HTTPS)
+        self.protocol: str = kwargs.pop('protocol', QDRANT_PROTOCOL)
+        self.conn_type: str = kwargs.pop('conn_type', QDRANT_CONN_TYPE)
+        self.url: str = kwargs.pop('url', QDRANT_URL)
+        self._host: str = f"{self.protocol}://{self.host}:{self.port}"
+
+    async def connection(self):
+        """Initialize Qdrant vector store.
+
+        Connects to a Qdrant instance, if connection fails, raise error.
         """
-        Creates a Qdrant client based on the provided configuration.
-
-        Args:
-            host: Host of the Qdrant server (if using "server" connection).
-            port: Port of the Qdrant server (if using "server" connection).
-            url: URL of the Qdrant cloud service (if using "cloud" connection).
-            https: Whether to use HTTPS for the connection.
-            verify: Whether to verify the SSL certificate.
-            qdrant_args: Additional arguments for the Qdrant client.
-
-        Returns:
-            A QdrantClient object.
-        """
-        if url is not None:
-            return QdrantClient(
-            url=url,
-            port=None,
-            verify=verify,
-            **qdrant_args
+        try:
+            self._connection = QdrantClient(
+                host=self.host,
+                port=self.port,
             )
-        else:
-            return QdrantClient(
-            host,
-            port=port,
-            https=https,
-            verify=verify,
-            **qdrant_args
-            )
-
-    def __init__(self, embeddings: Union[str, Callable] = None, **kwargs):
-        super().__init__(embeddings, **kwargs)
-        self.host = kwargs.get("host", QDRANT_HOST)
-        self.port = kwargs.get("port", QDRANT_PORT)
-        qdrant_args = kwargs.get("qdrant_args", {})
-        self.connection_type = kwargs.get("connection_type", QDRANT_CONN_TYPE)
-        url = kwargs.get("url", QDRANT_URL)
-        if url is not None:
-            self.url = url
-        else:
-            self.url = f"{QDRANT_PROTOCOL}://{self.host}"
-            if self.port:
-                self.url += f":{self.port}"
-        self.qdrant_args = {**qdrant_args, **kwargs}
-
-    def connection(self) -> Callable:
-        """Connects to the Qdrant database."""
-        client = None
-        if self.connection_type == "server":
-            client = self._create_qdrant_client(
-                self.host, self.port, self.url, QDRANT_USE_HTTPS, False, self.qdrant_args
-            )
-        elif self.connection_type == "cloud":
-            if self.url is None:
-                raise ValueError(
-                    "A URL is required for 'cloud' connection"
+            collections = self._connection.get_collections().collections
+            collection_names = [collection.name for collection in collections]
+            if self.collection_name not in collection_names:
+                print(
+                    f"Collection '{self.collection_name}' not found, creating new one."
                 )
-            client = self._create_qdrant_client(
-                None, None, self.url, False, False, self.qdrant_args
+                self._connection.create_collection(
+                    collection_name=self.collection_name,
+                    vectors_config=models.VectorParams(
+                        size=len(self._embed_.embed_query("test")), distance=models.Distance.EUCLID
+                    )
+                )
+            self.client = Qdrant(
+                client=self._connection,
+                collection_name=self.collection_name,
+                embedding_function=self._embed_
             )
+        except Exception as e:
+            print(f"Error connecting to Qdrant: {e}")
+            raise
+        self._connected = True
+        return self._connection
+
+    async def disconnect(self) -> None:
+        """Disconnects from Qdrant instance."""
+        self.client = None # No explicit disconnect needed for QdrantClient in this context.
+        self._connection = None
+        self._connected = False
+
+    def get_vector(
+        self,
+        embedding: Optional[Callable] = None,
+    ) -> Qdrant:
+        """Returns Qdrant VectorStore instance."""
+        if embedding is not None:
+            _embed_ = embedding
         else:
-            raise ValueError(
-                f"Invalid connection type: {self.connection_type}"
-            )
-        return client
-
-
-    def get_vectorstore(self):
-        if self._embed_ is None:
             _embed_ = self.create_embedding(
                 embedding_model=self.embedding_model
             )
-        else:
-            _embed_ = self._embed_
-        self.vector = Qdrant(
+        if not self._connection:
+            # Re-establish client if lost, but should not happen frequently in well-managed context.
+            self._connection = QdrantClient(host=self.host, port=self.port)
+        return Qdrant(
+            client=self._connection,
+            collection_name=self.collection_name,
+            embedding_function=_embed_
+        )
+
+    async def from_documents(self, documents: list[Document], **kwargs):
+        """Save Documents as Vectors in Qdrant."""
+        vectordb = await Qdrant.afrom_documents(
+            documents=documents,
+            embedding=self._embed_,
             client=self.client,
             collection_name=self.collection_name,
-            embeddings=_embed_,
+            **kwargs
         )
-        return self.vector
+        return vectordb
 
-    def search(self, payload: dict, collection: str = None) -> dict:
-        pass
+    async def add_documents(
+        self,
+        documents: list[Document],
+        embedding: Optional[Callable] = None,
+    ) -> bool:
+        """Add Documents to Qdrant."""
+        async with self:
+            vector_db = self.get_vector(embedding=embedding)
+            await vector_db.aadd_documents(documents=documents)
+        return True
+
+    async def update_documents(
+        self,
+        documents: list[Document],
+        embedding: Optional[Callable] = None,
+    ) -> bool:
+        """Update Documents in Qdrant."""
+        async with self:
+            vector_db = self.get_vector(embedding=embedding)
+            if all('id' in doc for doc in documents):
+                ids = [doc.pop('id') for doc in documents]
+                vector_db.delete(ids=ids)  # Remove old entries
+                await vector_db.aadd_documents(documents=documents)  # Add new versions
+                return True
+            return False
+
+    async def similarity_search(
+        self,
+        query: str,
+        embedding: Optional[Callable] = None,
+        limit: int = 2,
+    ) -> list:
+        """Performs similarity search in Qdrant."""
+        async with self:
+            vector_db = self.get_vector(embedding=embedding)
+            return await vector_db.asimilarity_search(query, k=limit) # Use asimilarity_search for async
+
+    def memory_retriever(
+        self,
+        documents: Optional[list] = None,
+        num_results: int = 5
+    ) -> VectorStoreRetrieverMemory:
+        """Retrieves stored memory-based documents."""
+        if not documents:
+            documents = []
+        vectordb = Qdrant.from_documents(
+            documents=documents,
+            embedding=self._embed_,
+            client=self.client,
+            collection_name=self.collection_name
+        )
+        retriever = Qdrant.as_retriever(
+            vectordb,
+            search_kwargs=dict(k=num_results)
+        )
+        return VectorStoreRetrieverMemory(retriever=retriever)
