eaff80e815b68c464e8b1a02ddb4103b2fad1925
Jesus Lara <jesuslarag@gmail.com>
2025-03-01 13:43:07 +0100

    adding more tests

--- examples/test_store.py
+++ examples/test_store.py
@@ -3,6 +3,8 @@ from langchain.docstore.document import Document
 from navconfig import BASE_DIR
 from parrot.stores.faiss import FaissStore
 from parrot.stores.chroma import ChromaStore
+from parrot.stores.duck import DuckDBStore
+from parrot.stores.postgres import PgvectorStore
 
 
 embed_model = {
@@ -16,6 +18,11 @@ async def test_store(use: str):
         _store = FaissStore(index_path=BASE_DIR.joinpath("faiss_index"), embedding_model=embed_model)
     if use == 'chroma':
         _store = ChromaStore(embedding_model=embed_model, ephemeral=True)
+    if use == 'duck':
+        _store = DuckDBStore(embedding_model=embed_model, database=":memory:")
+    if use == 'postgres':
+        _store = PgvectorStore(embedding_model=embed_model, dsn="postgresql+asyncpg://troc_pgdata:12345678@127.0.0.1:5432/navigator", drop=True)
+    print('Store selected: ', _store)
     # Create two LangChain documents
     doc1 = Document(page_content="LangChain is a framework for building language models.")
     doc2 = Document(page_content="Milvus is a vector database for efficient similarity search.")
@@ -32,4 +39,4 @@ async def test_store(use: str):
             print(result)
 
 if __name__ == "__main__":
-    asyncio.run(test_store(use='chroma'))
+    asyncio.run(test_store(use='postgres'))
