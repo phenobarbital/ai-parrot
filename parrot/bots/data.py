501d38deaa00b795241afdc09e6540a3a4f42ba3
Jesus Lara <jesuslarag@gmail.com>
2025-05-07 04:55:12 +0200

    wip: podcast tool

--- parrot/bots/data.py
+++ parrot/bots/data.py
@@ -27,13 +27,14 @@ from .prompts.data import (
     REACT_PROMPT_PREFIX,
     FORMAT_INSTRUCTIONS
 )
+from ..tools.gvoice import GoogleVoiceTool
 
-# Enable Debug:
-# from langchain.globals import set_debug, set_verbose
+## Enable Debug:
+from langchain.globals import set_debug, set_verbose
 
-# # Enable verbosity for debugging
-# set_debug(True)
-# set_verbose(True)
+# Enable verbosity for debugging
+set_debug(True)
+set_verbose(True)
 
 
 def brace_escape(text: str) -> str:
@@ -140,6 +141,8 @@ class PandasAgent(BasicAgent):
         dfs = list(self.df.values()) if isinstance(self.df, dict) else self.df
         # bind the tools to the LLM:
         # python_tool = PythonAstREPLTool(locals=self.df_locals, verbose=True,)
+        print('TOOLS > ')
+        print(self.tools)
         llm = self._llm.bind_tools(self.tools)
         return create_pandas_dataframe_agent(
             llm,
@@ -147,7 +150,11 @@ class PandasAgent(BasicAgent):
             verbose=True,
             agent_type=self.agent_type,
             allow_dangerous_code=True,
-            extra_tools=self.tools,
+            extra_tools=[
+                GoogleVoiceTool(
+                    name="generate_podcast_style_audio_file"
+                )
+            ],
             prefix=self._prompt_prefix,
             max_iterations=10,
             agent_executor_kwargs={"memory": self.memory, "handle_parsing_errors": True},
@@ -288,6 +295,8 @@ class PandasAgent(BasicAgent):
         # Add EDA functions to the tool's locals
         setup_code = """
         from parrot.bots.tools import quick_eda, generate_eda_report, list_available_dataframes, create_plot, generate_pdf_from_html
+        import statsmodels.api as sm
+        import seaborn as sns
         """
         # Add a helper function to the REPL locals
         setup_code += """
