aab96ffeb1f3ce18da44bc512d4cb1598a792790
Jesus Lara <jesuslarag@gmail.com>
2025-07-27 11:46:39 +0200

    test vector store

--- examples/askbuddy/load_pdf.py
+++ examples/askbuddy/load_pdf.py
@@ -2,29 +2,19 @@
 Load RFP documents into bot.
 """
 import asyncio
-from navconfig import BASE_DIR
 from pprint import pprint
 from pathlib import Path
-from parrot.chatbots.basic import Chatbot
-from parrot.llms.vertex import VertexLLM
+from parrot.bots.basic import BasicBot
 from parrot.loaders import (
     PDFLoader
 )
 
 async def get_agent():
-    llm = VertexLLM(
-        model='gemini-1.5-pro',
-        temperature=0.1,
-        top_k=30,
-        Top_p=0.5,
-    )
-    agent = Chatbot(
-        name='AskBuddy',
-        llm=llm
+    agent = BasicBot(
+        name='AskBuddy'
     )
     # Add LLM
     await agent.configure()
-    # directory = BASE_DIR.joinpath("docs", "askbuddy")
     directory = Path('/home/jesuslara/proyectos/navigator/navigator-ai').joinpath("docs", "askbuddy")
     for filename in directory.glob('*.pdf'):
         # Loading File by File to avoid overheat in database
@@ -33,14 +23,13 @@ async def get_agent():
         loader = PDFLoader(
             filename,
             source_type=f"MSO {filename.name}",
-            llm=llm.get_llm(),
             language="en",
             parse_images=False,
             page_as_images=True
         )
-        docs = loader.load()
+        docs = await loader.load()
         pprint(docs)
-        await agent.load_documents(
+        await agent.store.add_documents(
             docs
         )
     return agent
