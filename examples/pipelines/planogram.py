5fcde3030c585bf88300d12cb6b3e8ef7c2c09ac
Jesus Lara <jesuslarag@gmail.com>
2025-08-31 17:03:12 +0200

    test

--- examples/pipelines/planogram.py
+++ examples/pipelines/planogram.py
@@ -117,6 +117,8 @@ async def main():
     # Endcap photo:
     image_path = BASE_DIR / "examples" / "pipelines" / "250714 BBY 501 Kennesaw GA.jpg"
     # image_path = BASE_DIR / "examples" / "pipelines" / "original_0.jpg"
+    # image_path = BASE_DIR / "examples" / "pipelines" / "06668994-c27e-44d9-8d59-f1f65559c2e1-recap.jpeg"
+    # image_path = BASE_DIR / "examples" / "pipelines" / "eb04d624-a180-4e5c-b592-ab0d40b558f9-recap.jpeg"
 
 
     # Run complete pipeline
