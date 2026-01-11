ddffbcbfae48331fc4bdbb4c60ddc794e489b3d1
Jesus Lara <jesuslarag@gmail.com>
2026-01-11 22:56:22 +0100

    bump version

--- parrot/version.py
+++ parrot/version.py
@@ -3,7 +3,7 @@
 __title__ = "ai-parrot"
 __description__ = "Live Chatbots based on Langchain chatbots and Agents \
     Integrated into Navigator Framework or used into any aiohttp applications."
-__version__ = "0.18.0"
+__version__ = "0.18.1"
 __author__ = "Jesus Lara"
 __author_email__ = "jesuslarag@gmail.com"
 __license__ = "MIT"
