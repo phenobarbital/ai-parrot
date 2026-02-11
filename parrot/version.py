cfb3200892e12c854f5c04126b43e83b785ee755
Jesus Lara <jesuslarag@gmail.com>
2026-02-11 22:53:09 +0100

    login page

--- parrot/version.py
+++ parrot/version.py
@@ -2,7 +2,7 @@
 
 __title__ = "ai-parrot"
 __description__ = "Complete Framework for AI Chatbots and Agents, Supporting A2A, MCP, RAG and more."
-__version__ = "0.21.3"
+__version__ = "0.21.4"
 __author__ = "Jesus Lara"
 __author_email__ = "jesuslarag@gmail.com"
 __license__ = "MIT"
