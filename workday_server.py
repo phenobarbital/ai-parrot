085c1c85b6087fab6c75136d3c6747f5c4ca5ed3
phenobarbital <jesuslarag@gmail.com>
2026-02-24 23:32:13 +0100

    Delete workday_server.py

--- workday_server.py
+++
@@ -1,9 +0,0 @@
-from parrot.services.mcp import ParrotMCPServer
-from parrot.tools.workday import WorkdayToolkit
-
-mcp = ParrotMCPServer(
-    name="workday-mcp",
-    tools={
-        "workday": WorkdayToolkit(redis_url="redis://localhost:6379/4")
-    }
-)
